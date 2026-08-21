9f1ce61f17671efaf6be7b7ef8ba57c682fc0c7f
Victor Groux <victorgroux99@gmail.com>
2026-08-21 09:26:30 +0200

    Added a verbose management for the header line (include the id)
    Correction of a possible division by 0 in case no packet was send

--- src/print.c
+++ src/print.c
@@ -5,7 +5,10 @@
 
 void	print_header(char* hostname, char* ip_resolved, t_config config)
 {
-	printf("PING %s (%s): %ld data bytes\n", hostname, ip_resolved, config.payload_size);
+	if (config.verbose)
+		printf("PING %s (%s): %ld data bytes, id 0x%x = %d\n", hostname, ip_resolved, config.payload_size, config.identifier, config.identifier);
+	else
+		printf("PING %s (%s): %ld data bytes\n", hostname, ip_resolved, config.payload_size);
 }
 
 void	print_packet(char* ip_resolved, int bytes_received, int id_sequence, int ttl_received, float rtt)
@@ -18,12 +21,15 @@ void	print_packet(char* ip_resolved, int bytes_received, int id_sequence, int tt
 
 void	print_stats(char* hostname, t_stats stats)
 {
+	long	loss = 0;
+	if (stats.transmitted != 0)
+		loss = (stats.transmitted - stats.received) * 100 / stats.transmitted;
 	printf("--- %s ping statistics ---\n", hostname);
-	printf("%ld packets transmitted, %ld packets received, %ld%% packet loss\n", stats.transmitted, stats.received, (stats.transmitted - stats.received) * 100 / stats.transmitted);
+	printf("%ld packets transmitted, %ld packets received, %ld%% packet loss\n", stats.transmitted, stats.received, loss);
 
-	if (stats.received != 0 && stats.min != 0.0)
+	if (stats.received != 0 && stats.max != 0.0)
 	{
-		double	avg = stats.sum / stats.transmitted;
+		double	avg = stats.sum / stats.received;
 		double	variance = (stats.sum_squared / stats.received) - (avg * avg);
 		printf("round-trip min/avg/max/stddev = %.3f/%.3f/%.3f/%.3f ms\n", stats.min, avg, stats.max, sqrt(variance));
 	}
